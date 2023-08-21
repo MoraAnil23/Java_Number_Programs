e7491667164cdf5e6450a4ec90a4041f5268122b
MoraAnil23 <132802330+MoraAnil23@users.noreply.github.com>
2023-08-21 16:38:52 +0530

    Add files via upload

---
+++ Even_fib_summation.java
@@ -0,0 +1,25 @@
+class Even_fib_summation 
+{
+	public static void main(String[] args) 
+	{
+		int a=0,b=1,c=0,sum=0;
+        
+		for (int i=1;i<=15 ; i++)
+		{
+			
+			if (c%2==0)
+			{
+				sum+=c;
+				System.out.println("the even fib numbers are:"+c);
+				
+				
+			}
+			a=b;
+			b=c;
+			c=a+b;
+			
+		}
+		
+		System.out.println("the sum of even fib:"+sum);
+	}
+}
