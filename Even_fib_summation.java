c86de8ea7a98d1f771abe8e0dd0385d4f5186a8f
MoraAnil23 <132802330+MoraAnil23@users.noreply.github.com>
2023-08-21 16:42:24 +0530

    Update Even_fib_summation.java

--- Even_fib_summation.java
+++ Even_fib_summation.java
@@ -3,23 +3,19 @@ class Even_fib_summation
 	public static void main(String[] args) 
 	{
 		int a=0,b=1,c=0,sum=0;
-        
+              // Summation of even fibnocci numbers 
 		for (int i=1;i<=15 ; i++)
 		{
 			
 			if (c%2==0)
 			{
-				sum+=c;
-				System.out.println("the even fib numbers are:"+c);
-				
-				
+		           sum+=c; 
+			   System.out.println("the even fib numbers are:"+c);
 			}
 			a=b;
 			b=c;
 			c=a+b;
-			
 		}
-		
-		System.out.println("the sum of even fib:"+sum);
+	         System.out.println("the sum of even fib:"+sum);
 	}
 }
